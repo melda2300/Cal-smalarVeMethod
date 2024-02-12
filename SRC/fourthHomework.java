0415e6968cd597a5530a484cfe8cfaae9161d2b6
melda <146105556+melda2300@users.noreply.github.com>
2024-02-13 01:46:04 +0300

    Update fourthHomework.java

--- SRC/fourthHomework.java
+++ SRC/fourthHomework.java
@@ -1,7 +1,4 @@
-package SRC;
-
 import java.util.Arrays;
-
 public class fourthHomework {
     // 1- Java'da bir metindeki her kelimenin ilk harfini büyük yapacak bir fonksiyon yazınız.
     public static String ilkHarfiniBuyuk( String kelimeler ) {
